d94a626c88d3695cf063e6a69b0d573af555606b
drumovski <jason.stacy@gmail.com>
2021-06-16 11:36:15 +1000

    add difference of squares

---
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -0,0 +1,21 @@
+export class Squares {
+  constructor(n) {
+    this.n = n;
+  }
+
+  get sumOfSquares() {
+    let sum = 0;
+    for (let i = 0; i <= this.n; i++) sum += i ** 2;
+    return sum;
+  }
+
+  get squareOfSum() {
+    let sum = 0;
+    for (let i = 0; i <= this.n; i++) sum += i;
+    return sum ** 2;
+  }
+
+  get difference() {
+    return this.squareOfSum - this.sumOfSquares;
+  }
+}
